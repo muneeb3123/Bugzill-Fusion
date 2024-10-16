018a81b0d1ef50f4258f0a02af229ea527da117c
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-10-16 20:57:37 +0500

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-User.create(name: 'Talha', email: 'manager@gmail.com', password: '123456', password_confirmation: '123456', user_type: 'manager')
+User.create(name: 'manager', email: 'manager@gmail.com', password: '123456', password_confirmation: '123456', user_type: 'manager')
+User.create(name: 'developer', email: 'developer@gmail.com', password: '123456', password_confirmation: '123456', user_type: 'developer')
+User.create(name: 'qa', email: 'qa@gmail.com', password: '123456', password_confirmation: '123456', user_type: 'qa')
